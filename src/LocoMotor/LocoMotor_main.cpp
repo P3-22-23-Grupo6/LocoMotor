07aeee3f636b3b4dbe8208d134396197f94ded3c
Javier V <javilleg@ucm.es>
2023-02-21 10:55:43 +0100

    editorconfig funcional

--- src/LocoMotor/LocoMotor_main.cpp
+++ src/LocoMotor/LocoMotor_main.cpp
@@ -5,27 +5,26 @@
 #include "OgreManager.h"
 #include "AudioManager.h"
 
-int exec();
-int initBullet();
+int exec ();
+int initBullet ();
 
-int main()
-{
-    AudioManager::Get()->AddSound("si", "si.wav");
-    AudioManager::Get()->PlaySound("si");
+int main () {
+	AudioManager::Get ()->AddSound ("si", "si.wav");
+	AudioManager::Get ()->PlaySound ("si");
 
-    OgreWrapper::OgreManager::init("Prueba");
-    OgreWrapper::OgreManager* man = OgreWrapper::OgreManager::getInstance();
-    man->createScene("Escena");
-    man->createScene("Escena2");
+	OgreWrapper::OgreManager::init ("Prueba");
+	OgreWrapper::OgreManager* man = OgreWrapper::OgreManager::getInstance ();
+	man->createScene ("Escena");
+	man->createScene ("Escena2");
 
-    OgreWrapper::Scene* x = man->getScene("Escenah");
-    std::cout << (x == nullptr ? "null\n" : "jiji\n");
-    //exec();
-    initBullet();
-    // exec();
-    man->render();
+	OgreWrapper::Scene* x = man->getScene ("Escenah");
+	std::cout << (x == nullptr ? "null\n" : "jiji\n");
+	//exec();
+	initBullet ();
+	// exec();
+	man->render ();
 
-    char basura;
-    std::cin >> basura;
-    return 0;
+	char basura;
+	std::cin >> basura;
+	return 0;
 }
