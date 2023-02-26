629df2fc59eed967e93ba7783178659d88f67c65
dcasiano <82372508+dcasiano@users.noreply.github.com>
2023-02-26 15:24:30 +0100

    clase camera

--- src/LocoMotor_Ogre/cpp/Camera.cpp
+++ src/LocoMotor_Ogre/cpp/Camera.cpp
@@ -1,11 +1,33 @@
 #include "Camera.h"
 
-OgreWrapper::Camera::Camera(Ogre::Camera* camera, Ogre::SceneNode* node)
+#include <OgreCamera.h>
+#include <OgreViewport.h>
+#include <OgreSceneNode.h>
+#include <OgreSceneManager.h>
+#include "OgreManager.h"
+#include <OgreRenderWindow.h>
+
+OgreWrapper::Camera::Camera(Ogre::Camera* camera, Ogre::SceneNode* node):Node(node)
 {
-	mNode = node;
 	mCamera = camera;
+	mCamera->setNearClipDistance (1);
+	mCamera->setFarClipDistance (10000);
+	mCamera->setAutoAspectRatio (true);
+	//mCamera->setPolygonMode(Ogre::PM_WIREFRAME);
+
+	vp = OgreWrapper::OgreManager::GetInstance()->GetRenderWindow()->addViewport(mCamera);
+	vp->setBackgroundColour (Ogre::ColourValue (0.6, 0.7, 0.8));
 }
 
 OgreWrapper::Camera::~Camera()
 {
+}
+
+void OgreWrapper::Camera::LookAt(const Ogre::Vector3& targetPoint)
+{
+	_node->lookAt(targetPoint, Ogre::Node::TS_WORLD);
+}
+
+void OgreWrapper::Camera::Render () {
+	vp->update ();
 }
