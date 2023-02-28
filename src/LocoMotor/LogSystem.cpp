0a0d9f1bd5b43c9e45796e8b574416016e215213
GonzaloFdezMoreno <48910540+GonzaloFdezMoreno@users.noreply.github.com>
2023-02-28 10:16:08 +0100

    corrección del estilo en LogSystem.cpp

--- src/LocoMotor/LogSystem.cpp
+++ src/LocoMotor/LogSystem.cpp
@@ -1,49 +1,49 @@
 #include "LogSystem.h"
 
 
-LogSystem::LogSystem() {
-	init();
-	//creaEscribe();
+LogSystem::LogSystem () {
+	Init ();
+	//Create ();
 }
 
-LogSystem::~LogSystem() {}
+LogSystem::~LogSystem () {}
 
-void LogSystem::init() {
+void LogSystem::Init () {
 
-	fopen_s(&file, "log.txt", "w+");
+	fopen_s (&_file, "log.txt", "w+");
 }
 
-void LogSystem::fileclose() {
-	fclose(file);
+void LogSystem::FileClose () {
+	fclose (_file);
 }
 
 
 
 //void guarda(int type, std::string message) {
 //void guarda(int type, std::string message,FILE* file) {
-void LogSystem::guarda(int type, std::string message) {
+void LogSystem::Save (int type, std::string message) {
 	/*if (type == 0)
-		messages.error.push_back("Error: " + message);
+		messages.error.push_back ("Error: " + message);
 	else if (type == 1) {
-		messages.others.push_back("Other: " + message);
+		messages.others.push_back ("Other: " + message);
 	}*/
 
 
 	if (type == 0) {
 		std::string errorStr = "Error: " + message + "\n";
-		fprintf(file, errorStr.c_str());
+		fprintf (_file, errorStr.c_str ());
 	}
 	else if (type > 1) {
 		std::string warnStr = "Warning: " + message + "\n";
-		fprintf(file, warnStr.c_str());
+		fprintf (_file, warnStr.c_str ());
 	}
 
 }
 
 
 //primera ver
-//void LogSystem::creaEscribe(std::ofstream& file) {
-//	for (int i = 0; i < messages.error.size(); i++) {
+//void LogSystem::Create (std::ofstream& file) {
+//	for (int i = 0; i < messages.error.size (); i++) {
 //		file << messages.error[i] << "\n";
 //	}
 //
