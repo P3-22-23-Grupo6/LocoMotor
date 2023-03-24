fb4fcd1958e7194248495439e5b01410dd8fa587
dcasiano <82372508+dcasiano@users.noreply.github.com>
2023-03-24 10:12:26 +0100

    factory de audiosource y audiolistener

---
+++ src/LocoMotor/AudioListenerFactory.cpp
@@ -0,0 +1,14 @@
+#include "AudioListenerFactory.h"
+#include "AudioListener.h"
+
+using namespace LocoMotor;
+
+LocoMotor::AudioListenerFactory::AudioListenerFactory() {
+}
+
+LocoMotor::AudioListenerFactory::~AudioListenerFactory() {
+}
+
+Component* LocoMotor::AudioListenerFactory::Create() {
+	return new AudioListener();
+}
