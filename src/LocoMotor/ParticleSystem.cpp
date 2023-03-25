25c42a670de1fc533a7709cc444c0503cadc0a72
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2023-03-25 13:24:59 +0100

    particlesystem made component now!

--- src/LocoMotor/ParticleSystem.cpp
+++ src/LocoMotor/ParticleSystem.cpp
@@ -1,164 +1,35 @@
 #include "ParticleSystem.h"
+#include "LocoMotor_Ogre/include/ParticleHelper.h"
+#include "RenderScene.h"
+#include "Scene.h"
+#include "GameObject.h"
+#include "Node.h"
 
 using namespace LocoMotor;
 
-ParticleSystem::ParticleSystem() {
-	_comp = nullptr;
-	_particleSystem = nullptr;
-	_node = nullptr;
-	_sceneManager = nullptr;
-	_entity = nullptr;
-	_position = Ogre::Vector3::ZERO;
-	_scale = Ogre::Vector3::ZERO;
-	_rotation = Ogre::Vector3::ZERO;
+const std::string ParticleSystem::name = "ParticleSystem";
+
+ParticleSystem::ParticleSystem(Scene* scene, OgreWrapper::RenderScene* renderScn) {
+	_scene = scene;
+	_renderScn = renderScn;
 }
 
-ParticleSystem::~ParticleSystem() {
+void ParticleSystem::InitComponent() {
+	//Crear nodo
+	_node = _renderScn->CreateNode("ParticleNode");
+	//Crear particulas
+	_particleHelper = _renderScn->CreateParticleHelper("ParticleHelper", _filename);
+	//Attachear al nodo del gameObject
+	OgreWrapper::RenderEntity* renderObj = (OgreWrapper::RenderEntity*) this;
+	gameObject->GetNode()->Attach(renderObj);
+	//SetNode al gameObject
 }
 
-void ParticleSystem::Init(std::string name) {
-	_sceneManager = 
-	_particleSystem = _sceneManager->createParticleSystem(_entity->getName() + "ParticleSystem", ("Examples/" + name));
-}
-
-//void ParticleSystem::Update(float dt) {
-//}
-//
-//void ParticleSystem::Render() {
-//
-//
-//}
-
-void ParticleSystem::AddEmitter(std::string name, LMVector3 position) {
-	if (_emitters.find(name) == _emitters.end()) {
-		_emitters.insert({ name, _particleSystem->addEmitter(name) });
-		_emitters[name]->setPosition(LMVector3::LmToOgre(position));
-	}
-	else {
-		int num = 0;
-		bool found = false;
-		std::string newName = name + std::to_string(num);
-		while (!found) {
-			
-			if (_emitters.find(newName) == _emitters.end()) {
-				_emitters.insert({ newName, _particleSystem->addEmitter(newName)});
-				_emitters[newName]->setPosition(LMVector3::LmToOgre(position));
-
-				found = true;
-			}
-			else {
-				num++;
-				newName = name + std::to_string(num);
-			}
-		}
+void ParticleSystem::Update(float dt) {
+	if (_node != nullptr) {
+		float x = gameObject->GetNode()->GetPosition_X();
+		float y = gameObject->GetNode()->GetPosition_Y();
+		float z = gameObject->GetNode()->GetPosition_Z();
+		_node->SetPosition(x, y, z);
 	}
 }
-
-void ParticleSystem::AddEmitter(LMVector3 position) {
-	std::string name = "unnamedEmitter";
-	AddEmitter(name, position);
-}
-
-void ParticleSystem::RemoveEmitter(std::string name) {
-	if (_emitters.find(name) != _emitters.end()) {
-		_particleSystem->removeEmitter(_emitters[name]);
-		_emitters.erase(name);
-	}
-	else {
-		int num = 0;
-		bool found = false;
-		std::string delName = name + std::to_string(num);
-		while (!found) {
-
-			if (_emitters.find(delName) == _emitters.end()) {
-				_particleSystem->removeEmitter(_emitters[delName]);
-				_emitters.erase(delName);
-
-				found = true;
-			}
-			else {
-				num++;
-				delName = name + std::to_string(num);
-			}
-		}
-	}
-}
-
-void ParticleSystem::RemoveEmitter() {
-	std::string name = "unnamedEmitter";
-	RemoveEmitter(name);
-}
-
-Ogre::ParticleEmitter* ParticleSystem::GetEmitter(std::string name) {
-	if (_emitters.find(name) != _emitters.end()) {
-		return _emitters[name];
-	}
-	else {
-		int num = 0;
-		bool found = false;
-		std::string newName = name + std::to_string(num);
-		while (!found) {
-
-			if (_emitters.find(newName) != _emitters.end()) {
-				found = true;
-				return _emitters[newName];
-			}
-			else {
-				num++;
-				newName = name + std::to_string(num);
-			}
-		}
-	}
-}
-
-
-
-void ParticleSystem::SetContext(GameObject* ent) {
-	_comp = ent->GetComponent<LocoMotor::ParticleSystem>();
-}
-
-void ParticleSystem::SetPosition(LMVector3 position) {
-	_position = LMVector3::LmToOgre(position);
-	_node->setPosition(_position);
-}
-
-void ParticleSystem::SetDirection(LMVector3 direction) {
-	_node->setDirection(LMVector3::LmToOgre(direction));
-}
-
-void ParticleSystem::SetRotation(LMVector3 rotation) {
-	_rotation = LMVector3::LmToOgre(rotation);
-	_node->setOrientation(Ogre::Quaternion(Ogre::Degree(_rotation.x), Ogre::Vector3::UNIT_X) * Ogre::Quaternion(Ogre::Degree(_rotation.y), Ogre::Vector3::UNIT_Y) * Ogre::Quaternion(Ogre::Degree(_rotation.z), Ogre::Vector3::UNIT_Z));
-}
-
-void ParticleSystem::SetRotation(LMQuaternion rotation) {
-	_node->setOrientation(LMQuaternion::LmToOgre(rotation));
-}
-
-void ParticleSystem::SetScale(LMVector3 scale) {
-	_scale = LMVector3::LmToOgre(scale);
-	_node->setScale(_scale);
-}
-
-void ParticleSystem::SetVelocity(LMVector3 velocity) {
-	_particleSystem->setVelocity(LMVector3::LmToOgre(velocity));
-}
-
-void ParticleSystem::SetVelocity(LMVector3 axis, float minVelocity, float maxVelocity) {
-	_particleSystem->setVelocity(LMVector3::LmToOgre(axis), minVelocity, maxVelocity);
-}
-
-void ParticleSystem::SetVisible(bool visible) {
-	_particleSystem->setVisible(visible);
-}
-
-void ParticleSystem::SetEmitting(bool emitting) {
-	_particleSystem->setEmitting(emitting);
-}
-
-void ParticleSystem::SetSpeed(float speed) {
-	_particleSystem->setSpeed(speed);
-}
-
-
-
--- src/LocoMotor/ParticleSystem.h
+++ src/LocoMotor/ParticleSystem.h
@@ -1,64 +1,32 @@
 #ifndef LM_PARTICLESYS
 #define LM_PARTICLESYS
 
-
 #include "Component.h"
-#include "Ogre.h"
-#include "GameObject.h"
-#include <string>
 
-namespace Particle {
+namespace OgreWrapper {
 	class ParticleHelper;
+	class Node;
+	class RenderScene;
 }
 
 namespace LocoMotor {
+	class Scene;
+	class GameObject;
 	class ParticleSystem : public Component {
 	public:
-		ParticleSystem();
-		~ParticleSystem();
-		void Init(std::string name);
-		void Update(float dt);
-		void Render();
-		void SetContext(GameObject* ent);
+		const static std::string name;
+		ParticleSystem(Scene* scene, OgreWrapper::RenderScene* renderScn);
+		OgreWrapper::Node* _node;
 
-		void AddEmitter(std::string name, LMVector3 position);
-		void AddEmitter(LMVector3 position);
+		void InitComponent() override;
 
-		Ogre::ParticleEmitter* GetEmitter(std::string name);
-
-		void RemoveEmitter(std::string name);
-		void RemoveEmitter();
-		
-		void SetPosition(LMVector3 position);
-
-		void SetDirection(LMVector3 direction);
-
-		void SetRotation(LMVector3 rotation);
-		void SetRotation(LMQuaternion rotation);
-
-		void SetScale(LMVector3 scale);
-
-		void SetVelocity(LMVector3 velocity);
-		void SetVelocity(LMVector3 axis, float minVelocity, float maxVelocity);
-
-		void SetVisible(bool visible);
-
-		void SetEmitting(bool emitting);
-		void SetSpeed(float speed);
-
-		void SetAcceleration(LMVector3 axis, float acceleration);
-		void SetAcceleration(LMVector3 axis, float minAcceleration, float maxAcceleration);
-		
+		void Update(float dt) override;
 	private:
-		Component* _comp;
-		Ogre::ParticleSystem* _particleSystem;
-		Ogre::SceneNode* _node;
-		Ogre::SceneManager* _sceneManager;
-		Ogre::Entity* _entity;
-		Ogre::Vector3 _position;
-		Ogre::Vector3 _scale;
-		Ogre::Vector3 _rotation;
-		std::map <std::string, Ogre::ParticleEmitter*> _emitters;
+		Scene* _scene;
+		OgreWrapper::RenderScene* _renderScn;
+		OgreWrapper::ParticleHelper* _particleHelper;
+
+		std::string _filename;
 	};
 }
 
