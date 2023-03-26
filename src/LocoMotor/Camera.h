18953cca3ac312b528a2ae75e688f1114965ea37
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-03-26 17:37:15 +0200

    Funcionamiento de la camara correcto usando el gameObject desde el update

--- src/LocoMotor/Camera.h
+++ src/LocoMotor/Camera.h
@@ -4,6 +4,7 @@
 //	class Component;
 //}
 #include "Component.h"
+#include "lmVector.h"
 
 namespace OgreWrapper {
 	class RenderScene;
@@ -18,16 +19,23 @@ namespace LocoMotor {
 	public:
 		const static std::string name;
 
-		Camera(Scene* _scene, OgreWrapper::RenderScene* _renderScn);
+		// Para crear la camara se necesita tanto la escena como la escena de Render
+		// 
+		Camera(Scene* scene, OgreWrapper::RenderScene* renderScn, GameObject* target = nullptr, LMVector3 offset = LMVector3(0, 0, 0));
 
 		OgreWrapper::Node* _node;
 
 		void InitComponent() override;
 		void Update(float dt) override;
 
+		void SetTarget(GameObject* target, LMVector3 offset);
+
 	private:
 		Scene* _scene;
 		OgreWrapper::RenderScene* _renderScn;
+
+		GameObject* _target;
+		LMVector3 _offset;
 
 		//GetCamera();
 	};
