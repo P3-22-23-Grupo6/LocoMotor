0122b51e8d4410fab3051a2ec0ca77a06a5f078b
Javier <javilleg@ucm.es>
2023-03-30 22:38:10 +0200

    se puede cargar implicita o explicitamente
    y mas limpio

--- src/LM_Main/LocoMotor_main.cpp
+++ src/LM_Main/LocoMotor_main.cpp
@@ -16,12 +16,12 @@
 #include "GameObject.h"
 
 #include "windows.h"
+#include "MotorApi.h"
 
-#include "../projects/LocoMotor/MotorApi.h"
 // typedef de los metodos que vamos a encontrar en la dll del Juego
 typedef const char* (CALLBACK* InitJuegoFunc)();
 
-typedef void* (CALLBACK* InitJuegoFunc)();
+typedef void (CALLBACK* InitMotor)();
 
 int exec();
 using namespace PhysicsWrapper;
@@ -67,46 +67,48 @@ int main() {
 		std::cerr << "DLL EXPLICIT LOADING ERROR: '" << dllName << "' wasn't found" << std::endl;
 	}
 
-//#pragma region Explicit dll loading
-//
-//	HINSTANCE motorDLL;
-//	InitJuegoFunc initMotor;
-//
-//	LPCWSTR dllName;
-//
-//	// Esa 'L' antes del string es un prefijo que permite pasar los caracteres de una string a "long char" o "wide char"
-//	// (chars que utilizan 2 bytes para guardarse en vez de 1), que es lo que usa LPCWSTR (long pointer to constant WIDE string)
-//#ifdef _DEBUG
-//	dllName = L"LocoMotor_d";
-//#else
-//	dllName = L"LocoMotor";
-//#endif // _DEBUG
-//
-//	// Cargamos la libreria
-//	motorDLL = LoadLibrary(dllName);
-//
-//	if (motorDLL != NULL) {
-//
-//		// Buscamos la funcion, notese que no hace falta hacer lo de Wide Char
-//		LPCSTR functionName = LPCSTR("InitJuego");
-//		initJuego = (InitJuegoFunc) GetProcAddress(motorDLL, functionName);
-//
-//		if (initJuego != NULL) {
-//			// La ejecutamos
-//			const char* result = initJuego();
-//			std::cout << result << std::endl;
-//		}
-//		else {
-//			std::cerr << "DLL EXPLICIT LOADING ERROR: '" << functionName << "' function couldn't be executed" << std::endl;
-//		}
-//		FreeLibrary(motorDLL);
-//	}
-//	else {
-//		std::cerr << "DLL EXPLICIT LOADING ERROR: '" << dllName << "' wasn't found" << std::endl;
-//	}
-	
+#pragma region Explicit dll loading
+
+	HINSTANCE motorDLL;
+	InitMotor initMotor;
+
+	LPCWSTR dllNamem;
+
+	// Esa 'L' antes del string es un prefijo que permite pasar los caracteres de una string a "long char" o "wide char"
+	// (chars que utilizan 2 bytes para guardarse en vez de 1), que es lo que usa LPCWSTR (long pointer to constant WIDE string)
+#ifdef _DEBUG
+	dllNamem = L"LocoMotor_d";
+#else
+	dllNamem = L"LocoMotor";
+#endif  _DEBUG
+
+	// Cargamos la libreria
+	motorDLL = LoadLibrary(dllNamem);
+
+	if (motorDLL != NULL) {
+
+		// Buscamos la funcion, notese que no hace falta hacer lo de Wide Char
+		LPCSTR functionName = LPCSTR("init");
+		initMotor = (InitMotor) GetProcAddress(motorDLL, functionName);
+
+		if (initMotor != NULL) {
+			// La ejecutamos
+			initMotor();
+		}
+		else {
+			std::cerr << "DLL EXPLICIT LOADING ERROR: '" << functionName << "' function couldn't be executed" << std::endl;
+		}
+		FreeLibrary(motorDLL);
+	}
+	else {
+		std::cerr << "DLL EXPLICIT LOADING ERROR: '" << dllNamem << "' wasn't found" << std::endl;
+	}
+
+	/*
 	MotorApi* a = new MotorApi();
 	a->init();
 	delete a;
+	*/
+
 	return 0;
 }
