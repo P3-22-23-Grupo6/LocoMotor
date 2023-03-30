89303ca069f8fe659ad40beb714b6fc2b613f222
Dewei Chen <deweiche@ucm.es>
2023-03-30 20:05:01 +0200

    Create Clean.bat

---
+++ Clean.bat
@@ -0,0 +1,6 @@
+rmdir /s /q .\Dependecies\Bullet\Build
+rmdir /s /q .\Dependecies\Lua\build
+rmdir /s /q .\Dependecies\Lua\Temp
+rmdir /s /q .\Dependecies\Ogre\Build
+rmdir /s /q .\Dependecies\cegui-0.8.7\build
+rmdir /s /q .\Dependecies\cegui-0.8.7\cegui-dependencies\build
