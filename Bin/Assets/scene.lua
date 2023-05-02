fb52bfc36d7cb7bc17177f6fe0560f3fe9eb8905
Jacobo0601 <82810110+Jacobo0601@users.noreply.github.com>
2023-05-02 21:39:48 +0200

    Todo el mapa cargado por Lua

--- Bin/Assets/scene.lua
+++ Bin/Assets/scene.lua
@@ -18,6 +18,20 @@ coche = {
         friction = "0.0"
     }
 }
+mainCam = {
+    Transform = {
+        position = "0 0 0",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    Camera = 
+    { 
+        FOV = "60",
+        target = "coche" ,
+        main = ""
+    },
+    AudioListener = {}
+}
 do ---------------------   AREA01   ------------------------------
     Area01_RoadMain = {
         Transform = {
@@ -184,24 +198,80 @@ do ---------------------   AREA03   ------------------------------
     }
 
 end -------------------------------------------------------------------
-mainCam = {
+do ---------------------   AREA04   ------------------------------
+    Area04_Vegetation = {
+        Transform = {
+            position = "0 -6 0",
+            rotation = "0 0 0",
+            scale = "1 1 1"
+        },
+        MeshRenderer = {
+            file =  "Area04_Vegetation.mesh",
+            static = ""
+        }
+    }
+    Area04_Terrain = {
+        Transform = {
+            position = "0 -6 0",
+            rotation = "0 0 0",
+            scale = "1 1 1"
+        },
+        MeshRenderer = {
+            file =  "Area04_Terrain.mesh",
+            static = ""
+        }
+    }
+    Area04_Extra = {
+        Transform = {
+            position = "0 -6 0",
+            rotation = "0 0 0",
+            scale = "1 1 1"
+        },
+        MeshRenderer = {
+            file =  "Area04_Extra.mesh",
+            static = ""
+        }
+    }
+    Area05_Extra = {
+        Transform = {
+            position = "0 -6 0",
+            rotation = "0 0 0",
+            scale = "1 1 1"
+        },
+        MeshRenderer = {
+            file =  "Area05_Extra.mesh",
+            static = ""
+        }
+    }
+end -------------------------------------------------------------------
+Main_Terrain = {
     Transform = {
-        position = "0 0 0",
+        position = "0 -6 0",
         rotation = "0 0 0",
         scale = "1 1 1"
     },
-    Camera = 
-    { 
-        FOV = "60",
-        target = "coche" ,
-        main = ""
-    },
-    AudioListener = {}
+    MeshRenderer = {
+        file =  "Main_Terrain.mesh",
+        static = ""
+    }
 }
+Main_Water = {
+    Transform = {
+        position = "0 -6 0",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    MeshRenderer = {
+        file =  "Main_Water.mesh",
+        static = ""
+    }
+}
+
 
 entities = { "coche", "mainCam", 
             "Area01_RoadMain", "Area01_RoadBorder", "Area01_Vegetation", "Area01_Extra", "Area01_Terrain",
             "Area02_RoadMain", "Area02_RoadBorder", "Area02_Extra",
-            "Area03_RoadMain", "Area03_RoadBorder", "Area03_Vegetation", "Area03_Extra", "Area03_Terrain"
-        
+            "Area03_RoadMain", "Area03_RoadBorder", "Area03_Vegetation", "Area03_Extra", "Area03_Terrain",
+            "Area04_Vegetation", "Area04_Extra", "Area04_Terrain", "Area05_Extra",
+            "Main_Terrain", "Main_Water"
         }
