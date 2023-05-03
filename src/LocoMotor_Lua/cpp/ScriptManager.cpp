96db477418ce6978cf4b90625acfeddf80cd48f5
Dewei Chen <deweiche@ucm.es>
2023-05-03 15:11:29 +0200

    Documentación limpieza Locomotor Lua

--- src/LocoMotor_Lua/cpp/ScriptManager.cpp
+++ src/LocoMotor_Lua/cpp/ScriptManager.cpp
@@ -14,112 +14,106 @@ extern "C" {
 #include "LogSystem.h"
 using namespace LocoMotor;
 
-//bool LocoMotor::ScriptManager::Init() {
-//	
-//	return true;
-//}
 ScriptManager* Singleton<ScriptManager>::_instance = nullptr;
 
-luabridge::LuaRef ScriptManager::getFromLua(std::string name) {
-    return luabridge::getGlobal(luaState, name.c_str());
-
+luabridge::LuaRef ScriptManager::GetFromLua(std::string name) {
+	return luabridge::getGlobal(_luaState, name.c_str());
 }
 
-void ScriptManager::report_errors(int status) {
-    if (status == 0) {
-        return;
-    }
+void ScriptManager::Report_errors(int status) {
+	if (status == 0) {
+		return;
+	}
 
-    std::cerr << "[LUA ERROR] " << lua_tostring(luaState, -1) << std::endl;
+	std::cerr << "[LUA ERROR] " << lua_tostring(_luaState, -1) << std::endl;
 
-    // remove error message from Lua state
-    lua_pop(luaState, 1);
+	// remove error message from Lua state
+	lua_pop(_luaState, 1);
 }
 
-void ScriptManager::readLuaScript(const std::string path) {
-    
-    // load some code from Lua file
-    int scriptLoadStatus = luaL_dofile(luaState, path.c_str());
+void ScriptManager::ReadLuaScript(const std::string path) {
 
-    // define error reporter for any Lua error
-    report_errors(scriptLoadStatus);
+	// load some code from Lua file
+	int scriptLoadStatus = luaL_dofile(_luaState, path.c_str());
+
+	// define error reporter for any Lua error
+	Report_errors(scriptLoadStatus);
 }
+
 void ScriptManager::LoadSceneFromFile(std::string path) {
-    Scene* s = SceneManager::GetInstance()->GetSceneByName(path);
-    if (s == nullptr) {
-        s = scMan->CreateScene(path);
-    }
-    readLuaScript(path);
+	Scene* s = SceneManager::GetInstance()->GetSceneByName(path);
+	if (s == nullptr) {
+		s = _scMan->CreateScene(path);
+	}
+	ReadLuaScript(path);
 
-    luabridge::LuaRef allEnts = getFromLua("entities");
-    //luabridge::LuaRef layers = getFromLua("layers");
-    int numEnts = allEnts.length();
+	luabridge::LuaRef allEnts = GetFromLua("entities");
+	int numEnts = allEnts.length();
 
-    for (int i = 1; i <= numEnts; i++) {
-        luabridge::LuaRef entity = getFromLua(allEnts[i]);
+	for (int i = 1; i <= numEnts; i++) {
+		luabridge::LuaRef entity = GetFromLua(allEnts[i]);
 
-        //Cargo las entidades 
-        GameObject* ent = s->GetObjectByName(allEnts[i]);
-        std::cout << "Loading entity: " << allEnts[i] << " Components: \n";
-        if (!setParams(entity, ent, nullptr, "layer")) {
-            s->RemoveGameobject(allEnts[i]);
-        }
-    }
+		//Cargo las entidades 
+		GameObject* ent = s->GetObjectByName(allEnts[i]);
+		std::cout << "Loading entity: " << allEnts[i] << " Components: \n";
+		if (!SetParams(entity, ent, nullptr, "layer")) {
+			s->RemoveGameobject(allEnts[i]);
+		}
+	}
 
-    SceneManager::GetInstance()->ChangeScene(path);
+	SceneManager::GetInstance()->ChangeScene(path);
 }
 
 ScriptManager::ScriptManager() {
-    luaState = luaL_newstate();
-    scMan = SceneManager::GetInstance();
-    luaL_openlibs(luaState);
+	_luaState = luaL_newstate();
+	_scMan = SceneManager::GetInstance();
+	luaL_openlibs(_luaState);
 }
 
 LocoMotor::ScriptManager::~ScriptManager()
 {
-    lua_close(luaState);
-	//delete luaState;
+	lua_close(_luaState);
 }
 
-bool ScriptManager::setParams(luabridge::LuaRef entity, GameObject* ent, Scene* s, std::string layer)
+bool ScriptManager::SetParams(luabridge::LuaRef entity, GameObject* ent, Scene* s, std::string layer)
 {
 
-    lua_pushnil(entity);
-    if (entity.isNil()) {
-        LocoMotor::LogSystem::GetInstance()->Save(0, "LUA: Gameobject '" + ent->GetName() + "' has been declared but not defined");
-        return false;
-    }
-    while (lua_next(entity, 0) != 0) {
+	lua_pushnil(entity);
+	if (entity.isNil()) {
+		LocoMotor::LogSystem::GetInstance()->Save(0, "LUA: Gameobject '" + ent->GetName() + "' has been declared but not defined");
+		return false;
+	}
+	while (lua_next(entity, 0) != 0) {
 
 
-        std::string compName = lua_tostring(entity, -2);
-        std::cout << "       Loading component: " << compName << "\n";
-        std::string key;
+		std::string compName = lua_tostring(entity, -2);
+		std::cout << "       Loading component: " << compName << "\n";
+		std::string key;
 
-       luabridge::LuaRef component = entity[compName];
-       lua_pushnil(component);
-
-       
+		luabridge::LuaRef component = entity[compName];
+		lua_pushnil(component);
 
 
-        std::vector<std::pair<std::string, std::string>> parameters;
 
-        while (lua_next(component, 0) != 0) {
-            std::string key = lua_tostring(entity, -2);
-            std::string val = lua_tostring(entity, -1);
 
-            parameters.push_back({ key, val });
-            lua_pop(component, 1);
-        }
+		std::vector<std::pair<std::string, std::string>> parameters;
 
-        
-        ent->AddComponent(compName, parameters);
-        for (auto& p : parameters) {
-            std::cout << "\t" << p.first << ": " << p.second << "\n";
-        }
-        lua_pop(entity, 1);
-    }
-    if (ent->GetComponent<Transform>() == nullptr)
-        ent->AddComponent("Transform");
-    return true;
+		while (lua_next(component, 0) != 0) {
+			std::string key = lua_tostring(entity, -2);
+			std::string val = lua_tostring(entity, -1);
+
+			parameters.push_back({ key, val });
+			lua_pop(component, 1);
+		}
+
+
+		ent->AddComponent(compName, parameters);
+		for (auto& p : parameters) {
+			std::cout << "\t" << p.first << ": " << p.second << "\n";
+		}
+		lua_pop(entity, 1);
+	}
+	if (ent->GetComponent<Transform>() == nullptr)
+		ent->AddComponent("Transform");
+	return true;
 }
--- src/LocoMotor_Lua/include/ScriptManager.h
+++ src/LocoMotor_Lua/include/ScriptManager.h
@@ -15,27 +15,31 @@ namespace luabridge {
 	class LuaRef;
 }
 
-
 namespace LocoMotor {
 	class SceneManager;
 	class GameObject;
 	class Scene;
 	class MOTOR_API ScriptManager : public Singleton<ScriptManager> {
-	    friend Singleton<ScriptManager>;
+		friend Singleton<ScriptManager>;
 	public:
-		/*bool Init();*/
+		/// @brief Reads a lua file and builds the scene from that file
+		/// @param path The path to find the lua file
 		void LoadSceneFromFile(std::string path);
-		
 	private:
-		void readLuaScript(const std::string path);
-		void report_errors(int status);
-		luabridge::LuaRef getFromLua(std::string name);
-		bool setParams(luabridge::LuaRef entity, GameObject* ent, Scene* s, std::string layer);
+		/// @brief Reads the lua script 
+		/// @param path The path to find the lua script
+		void ReadLuaScript(const std::string path);
+		/// @brief Reports errors giving a status when reading lua
+		/// @param status 
+		void Report_errors(int status);
+		/// @brief Get a param from lua file giving the name of the param
+		luabridge::LuaRef GetFromLua(std::string name);
+		/// @brief Sets the param read from lua to the gameobject and scene
+		bool SetParams(luabridge::LuaRef entity, GameObject* ent, Scene* s, std::string layer);
 		ScriptManager();
 		~ScriptManager();
-
-		lua_State* luaState;
-		SceneManager* scMan;
+		lua_State* _luaState;
+		SceneManager* _scMan;
 
 	};
 }
