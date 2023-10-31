fea75d20e7bf7ab2257ee74c9b0c40f770be591b
Jacobo0601 <jacoboal@ucm.es>
2023-10-31 20:32:11 +0100

    Arreglo LUA ROTO

--- Bin/Assets/Scenes/trackSelectMenu.lua
+++ Bin/Assets/Scenes/trackSelectMenu.lua
@@ -35,5 +35,110 @@ selectTrackButton = {
         img = "m_select"
     }
 }
+
+goToCarButton = {
+    UIImageLM = { 
+        interactive = "",
+        posx = "0.02",
+        posy = "0.8",
+        sizex = "0.22",
+        sizey = "0.16",
+        img = "m_BackButton"
+    }
+}
+logoImg = {
+    UIImageLM = { 
+        interactive = "",
+        posx = "0.81",
+        posy = "0.01",
+        sizex = "0.2",
+        sizey = "0.18",
+        img = "LogoImg"
+    }
+}
+
+MenuBackgroundMesh = {
+    Transform = {
+        position = "0 0 -10",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    MeshRenderer = {
+        file =  "MenuBackgroundMesh.mesh"
+    }
+}
+
+mainCam = {
+    Transform = {
+        position = "0 0 6",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    Camera = 
+    { 
+        CameraMode = 0,
+        FOV = "34",
+        main = ""
+    }
+}
+trackArrowRightButton = {
+    UIImageLM = { 
+        interactive = "",
+        posx = "0.55",
+        posy = "0.4",
+        sizex = "0.08",
+        sizey = "0.13",
+        img = "ArrowRight00"
+    }
+}
+trackArrowLeftButton = {
+    UIImageLM = { 
+        interactive = "",
+        posx = "0",
+        posy = "0.4",
+        sizex = "0.08",
+        sizey = "0.13",
+        img = "ArrowLeft00"
+    }
+}
+trackMesh00 = {
+    Transform = {
+        position = "2 0 0",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    MeshRenderer = {
+        file =  "BlueFalcon_Out.mesh",
+        static = "true",
+        invisible = "true"
+    },
+    Rotator = {
+        Speed = "0.05"
+    }
+}
+trackMesh01 = {
+    Transform = {
+        position = "2 0 0",
+        rotation = "0 0 0",
+        scale = "1 1 1"
+    },
+    MeshRenderer = {
+        file =  "PreviewTrack00.mesh",
+        static = "true"
+        
+    },
+    Rotator = {
+        Speed = "0.05"
+    }
+}
+Portrait_Track = {
+    UIImageLM = { 
+        posx = "0.06",
+        posy = "0.14",
+        sizex = "0.5",
+        sizey = "0.6",
+        img = "Portrait_TrackOne"
+    }
+}
 entities = {"ButtonManager", "MenuDecalMesh", "goToIntroButton", "MenuBackgroundMesh", "falconPreview", "selectTrackTitle", "selectTrackButton",
 "mainCam", "logoImg", "menuBack", "audioHolder", "trackArrowLeftButton", "trackArrowRightButton", "trackMesh00", "trackMesh01", "Portrait_Track"}
