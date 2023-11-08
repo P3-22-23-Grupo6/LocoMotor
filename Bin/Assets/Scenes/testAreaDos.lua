d2dbaa7a4c3a5fff21bee418d9c30046d1869166
Jacobo0601 <jacoboal@ucm.es>
2023-11-08 22:13:23 +0100

    Limpieza, Comienzo Carrera, Respawn

--- Bin/Assets/Scenes/testAreaDos.lua
+++ Bin/Assets/Scenes/testAreaDos.lua
@@ -16,7 +16,7 @@ mainCam = {
 }
 coche = {
     Transform = {
-        position = "-5 4 10",
+        position = "1 1.75 19",
         rotation = "0 0 0",
         scale = "0.5 0.5 0.5"
     },
@@ -265,9 +265,9 @@ UnderWater = {
 --}
 --EnemyCar01 = {
 --    Transform = {
---        position = "0 4 -10",
+--        position = "1 2 -10",
 --        rotation = "0 0 0",
---        scale = "1 1 1"
+--        scale = "2 2 2"
 --    },
 --    MeshRenderer = {
 --        file =  "Pelican.mesh"
@@ -277,7 +277,7 @@ UnderWater = {
 --        playonStart = "Assets/Sounds/engine.wav"
 --    },
 --    EnemyAI = {
---        enemySpeed = "0.5",
+--        enemySpeed = "0.4",
 --        StartSeparation = "1"
 --    },
 --    RigidBodyComponent = {
@@ -453,38 +453,38 @@ NewWater = {
     Water = {
     }
 }
-wall00 = {
-    Transform = {
-        position = "0 -6 0",
-        rotation = "0 0 0",
-        scale = "1 1 1"
-    },
-    MeshRenderer = {
-        file =  "Wall.00.mesh",
-        static = "",
-        invisible = ""
-    },
-    RigidBodyComponent = {
-        mass = 0,
-        friction = "0.0"
-    }
-}
-wall01 = {
-    Transform = {
-        position = "0 -6 0",
-        rotation = "0 0 0",
-        scale = "1 1 1"
-    },
-    MeshRenderer = {
-        file =  "Wall.01.mesh",
-        static = "",
-        invisible = ""
-    },
-    RigidBodyComponent = {
-        mass = 0,
-        friction = "0.0"
-    }
-}
+--wall00 = {
+--    Transform = {
+--        position = "0 -6 0",
+--        rotation = "0 0 0",
+--        scale = "1 1 1"
+--    },
+--    MeshRenderer = {
+--        file =  "Wall.00.mesh",
+--        static = "",
+--        invisible = ""
+--    },
+--    RigidBodyComponent = {
+--        mass = 0,
+--        friction = "0.0"
+--    }
+--}
+--wall01 = {
+--    Transform = {
+--        position = "0 -6 0",
+--        rotation = "0 0 0",
+--        scale = "1 1 1"
+--    },
+--    MeshRenderer = {
+--        file =  "Wall.01.mesh",
+--        static = "",
+--        invisible = ""
+--    },
+--    RigidBodyComponent = {
+--        mass = 0,
+--        friction = "0.0"
+--    }
+--}
 entities = { "coche", "cocheDos", "mainCam", "secondCam", "velocityText", "consoleDebug", "Test_Track_00", "portrait01Img", "portrait02Img", "counter01Text",
 "Road00", "Road01", "Road02", "Road03", "Beach", 
 "Terrain00", "Terrain01", "Terrain02", "Terrain03", "waterPlane",
